45b0cf5ddf6a91b47841ada522c7cd2e31aaba0c
william Monthe <AMONTHE@graphicsvm.local>
2025-11-15 15:29:24 -0500

    premier commit

---
+++ bio.py
@@ -0,0 +1,11 @@
+while True :
+    try:
+        valeur = int(input("Entrez un nombre entre 1 et 100 :"))
+        if 1 <= valeur <= 100:
+            print ("valide !")
+            break
+        else:
+            print("le nombre doit etre 1 et 100.")
+    except ValueError:
+            print("Erreur :Entrez un nombre valide.")
+        
---
+++ kyo.py
@@ -0,0 +1,9 @@
+t=float(input("entrer un nombre"))
+if t>0 :
+    print("il est positif")
+elif t == 0 :
+      print("il est nul ")
+else :
+            print ("il est negatif")
+            
+
